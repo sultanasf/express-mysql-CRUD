7521ec292cc95ca9e5bd2443f72d04c80662658c
Sultan Firdaus <sultanfirdaus324@gmail.com>
2023-04-30 23:20:14 +0700

    nyoba sql

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2')
+require('dotenv').config()
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'biodata',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 })
 
 module.exports = pool.promise()
